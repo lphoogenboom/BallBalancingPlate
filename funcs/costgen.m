02b06753042d164e75432b7a963ff6faff870334
LHoogenboom <hoogenboom.l.p@gmail.com>
2021-04-06 15:19:18 +0200

    adds initial state to costgen function

--- funcs/costgen.m
+++ funcs/costgen.m
@@ -1,5 +1,5 @@
-function [H,h]=costgen(P,S,Q,R,dim)
-    Qbar=blkdiag(kron(eye(dim.N),Q),zeros(dim.nx)); 
+function [H,h]=costgen(T,S,Q,R,dim,x0)
+    Qbar=kron(eye(dim.N),Q); 
     H=S'*Qbar*S+kron(eye(dim.N),R);   
-    h=S'*Qbar*P;
+    h=S'*Qbar*T*x0;
 end
