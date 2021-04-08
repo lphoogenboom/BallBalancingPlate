340c2a7f405bbe60770127ae6d92c94445beb4ad
LHoogenboom <hoogenboom.l.p@gmail.com>
2021-04-08 10:09:18 +0200

    fixes typo throwing error

--- regulationMPC.m
+++ regulationMPC.m
@@ -71,7 +71,7 @@ end
 
 figure(1)
 stairs(T_1, x(1,:), 'b', 'LineWidth', 1.3);
-stairs(T_1, x(i,:), 'r', 'LineWidth', 1.3);
+stairs(T_1, x(3,:), 'r', 'LineWidth', 1.3);
 legend('xb', 'xy');
 
 figure(2)
